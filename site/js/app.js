3adbf9f9c47c0f9b4d99d380090bc573d2e27947
joao-delmilio <joao.delmilio14@gmail.com>
2025-05-30 17:57:26 -0300

    toques finais mas nao tao finais assim

--- site/js/app.js
+++ site/js/app.js
@@ -1,1 +1,4 @@
-console.log("teste")
+document.getElementById('form-agendamento').addEventListener('submit', function(event) {
+      event.preventDefault();
+      document.getElementById('mensagem').style.display = 'block';
+    });
